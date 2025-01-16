e23b457a19fbe3e12b12350ededbd0e22a730566
toryalai <toryalai.helali@gmail.com>
2025-01-16 12:09:57 -0500

    Dashboard added.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,6 +31,16 @@ const Header = () => {
                   Home
                 </Link>
                 <Link
+                  to="/dashboard"
+                  className={`inline-flex items-center px-1 pt-1 text-lg font-medium ${
+                    location.pathname === "/dashboard" 
+                      ? 'text-blue-600 border-b-2 border-blue-600' 
+                      : 'text-gray-500 hover:text-gray-700'
+                  }`}
+                >
+                  Dashboard
+                </Link>
+                <Link
                   to="/manage-events"
                   className={`inline-flex items-center px-1 pt-1 text-lg font-medium ${
                     location.pathname === "/manage-events" 
